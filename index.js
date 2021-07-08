e7a61b02736075b2f9ad03559e223c700c5c139e
SebastianMorel <theswedishteamkiller@gmail.com>
2021-07-08 19:10:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+//HAMBURGER BUTTON
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+$( document ).ready(function() {
+  $('.phone-navigation').toggle();
+});
+
+$('.menu-btn').on('click', function () {
+  $('.phone-navigation').toggle();
+});
+
+//SLIDESHOW
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
